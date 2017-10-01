5707a3c632b2824a419598012ea0279b65c462f3
shahid <shahid.dhariwala@gmail.com>
2017-10-02 01:09:15 +0530

    Fibonacci Series

---
+++ Java/JavaDemoPrograms/src/Fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+//Write a program to print fibonacci series.
+
+public class Fibonacci {
+	public static void main(String args[])
+	{
+		Scanner sz = new Scanner(System.in);
+		System.out.print("Enter the Number of Digits Required : ");
+		int num = sz.nextInt();
+		System.out.print("Fibonacci Series is as follow: \n");
+		System.out.print("0 \n1\n");
+		int a=0,b=1,c;
+		for(int i=0;i<num-2;i++)
+		{	
+			c=a+b;
+			System.out.println(c);
+			a=b;
+			b=c;
+		}
+		
+	}
+}
